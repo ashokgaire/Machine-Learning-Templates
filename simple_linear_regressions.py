1557b330db62d95570c1fc2378ba2a0ebe7b0f9d
oxy <ashokgaire1@gmail.com>
2020-11-15 16:58:02 +0545

    simple linear regressions

--- simple_linear_regressions.py
+++ simple_linear_regressions.py
@@ -1,2 +1,75 @@
-# -*- coding: utf-8 -*-
+# Simle linera Regressions (salary_predictor)
+#import 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+#import dataset
+datasets = pd.read_csv("data/Salary_Data.csv")
+X = datasets.iloc[:, :-1].values
+Y = datasets.iloc[:,1].values
+
+
+#spliting the data into train and test sets
+from sklearn.model_selection import train_test_split
+X_train , X_test, Y_train, Y_test = train_test_split(X,Y, test_size=1/3 , random_state=0)
+
+"""
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)"""
+
+
+
+#Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,Y_train)
+
+
+
+# Predicting the Test set results
+Y_pred = regressor.predict(X_test)
+
+
+# Visuaizing the Training set Results
+plt.scatter(X_train, Y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
